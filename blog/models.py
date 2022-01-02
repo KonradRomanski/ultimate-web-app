bfc4cdb3ac7a7878684e51b527e69fb2e5aaaafe
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2022-01-02 09:21:53 +0100

    add Stat and Comment Model

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=180)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    auth_user_id = models.ForeignKey(User)
-
-
 class Project(models.Model):
     name = models.CharField(max_length=180)
     github_link = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=180)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auth_user_id = models.ForeignKey(User)
+    project_id = models.ForeignKey(Project)
+
+
+class Stat(models.Model):
+    action_name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    last_action = models.DateTimeField()
+
+
+class Comment(models.Model):
+    description = models.TextField()
+    post_id = models.ForeignKey(Post)
+    auth_user_id = models.ForeignKey(User)
+
+
+
 
