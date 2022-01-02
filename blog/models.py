c5bd87379e1671aac691d535a3919ffcde3d110d
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2022-01-02 09:10:37 +0100

    add Post and Project Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=180)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auth_user_id = models.ForeignKey(User)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=180)
+    github_link = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
