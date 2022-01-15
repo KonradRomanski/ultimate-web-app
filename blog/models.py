8a8be8b246f32a50d1079cb3c58c43251c00cd59
Grzegorz Piatek <grzegorzadampiatek@gmail.com>
2022-01-15 23:32:22 +0100

    DB Migrate

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
 
 
 class Project(models.Model):
